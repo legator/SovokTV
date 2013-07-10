5790f65be4de917188ba633be5cb8ecf0b8bbdc5
oleg.legator <oleg.legator@gmail.com>
2013-07-10 19:25:06 +0300

    Source code of SovokTV player v0.0.4 alpha

---
+++ Player/WindowsStore/v0.0.4 alpha/SovokTV/View/EpgView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using SovokAPI.Class;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ApplicationSettings;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SovokTV
+{
+    public sealed partial class EpgView : UserControl
+    {
+        public EpgView()
+        {
+            this.InitializeComponent();
+            if (App.ViewModel.Active_Channel != null)
+                OpenEPG(App.ViewModel.Active_Channel.id);
+
+            ChannelEpgView.Height = Window.Current.Bounds.Height - 100;
+        }
+
+        private void HideSettings(object sender, RoutedEventArgs e)
+        {
+            if (this.Parent.GetType() == typeof(Popup))
+            {
+                ((Popup)this.Parent).IsOpen = false;
+            }
+            SettingsPane.Show();
+        }
+
+        private async Task OpenEPG(string id)
+        {
+            List<Programs> lp = new List<Programs>();
+            await App.ViewModel.GetEpg();
+            lp = App.ViewModel.EPG;
+            ChannelEpgView.ItemsSource = lp;
+        }
+    }
+}
---
+++ Player/WindowsStore/v0.0.4 alpha/SovokTV/View/SettingsView.xaml.cs
@@ -0,0 +1,92 @@
+﻿using SovokAPI.Class;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ApplicationSettings;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SovokTV
+{
+    public sealed partial class SettingsView : UserControl
+    {
+        public SettingsView()
+        {
+            this.InitializeComponent();
+            this.DataContext = App.ViewModel;
+            foreach (Streamers item in App.ViewModel.StreamServer)
+            {
+                ComboBoxItem boxitem = new ComboBoxItem();
+                boxitem.Content = item.id;
+                boxitem.Name = item.name;
+                Stream.Items.Add(boxitem);
+            }
+            set_value();
+        }
+
+        private void set_value()
+        {
+            BufferValue.Text = App.ViewModel.UserAccount.setting.Buffer;
+            DeinterlaceValue.Text = App.ViewModel.UserAccount.setting.Deinterlace;
+            int i = -1;
+            foreach (ComboBoxItem item in TimeZone.Items)
+            {
+                i++;
+                if (item.Content.ToString() == App.ViewModel.UserAccount.setting.TimeZone)
+                {
+                    break;
+                }
+            }
+            TimeZone.SelectedIndex = i;
+            i = -1;
+            foreach (ComboBoxItem item in Stream.Items)
+            {
+                i++;
+                if (item.Content.ToString() == App.ViewModel.UserAccount.setting.StreamServer)
+                {
+                    break;
+                }
+            }
+            Stream.SelectedIndex = i;
+        }
+
+        private void HideSettings(object sender, RoutedEventArgs e)
+        {
+            if (this.Parent.GetType() == typeof(Popup))
+            {
+                ((Popup)this.Parent).IsOpen = false;
+            }
+            SettingsPane.Show();
+        }
+
+        private void ChangedTimeZone(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem item = (ComboBoxItem)TimeZone.SelectedItem;
+            if (item.Content.ToString() == App.ViewModel.UserAccount.setting.TimeZone)
+            {
+                return;
+            }
+            App.ViewModel.SetTimeZone(item.Content.ToString());
+        }
+
+        private void ChangeStream(object sender, SelectionChangedEventArgs e)
+        {
+            Streamers st = App.ViewModel.StreamServer[Stream.SelectedIndex];
+            if (st.id == App.ViewModel.UserAccount.setting.TimeZone)
+            {
+                return;
+            }
+            App.ViewModel.SetStreamServer(Convert.ToInt32(st.id));
+        }
+    }
+}
